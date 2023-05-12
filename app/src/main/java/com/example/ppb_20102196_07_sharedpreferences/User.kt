4b64157412f8c6ebc15def8795e7666f0c271f9f
Tiara Humaira Putri <20102196@ittelkom-pwt.ac.id>
2023-05-12 08:37:25 +0700

    final commit

---
+++ app/src/main/java/com/example/ppb_20102196_07_sharedpreferences/User.kt
@@ -0,0 +1,3 @@
+package com.example.ppb_20102196_07_sharedpreferences
+
+data class User(val id: Int = -1, val email: String, val password: String)
---
+++ app/src/main/java/com/example/ppb_20102196_07_sharedpreferences/UserContract.kt
@@ -0,0 +1,12 @@
+package com.example.ppb_20102196_07_sharedpreferences
+
+import android.provider.BaseColumns
+
+object UserContract {
+    object UserEntry: BaseColumns{
+        const val TABLE_NAME = "user"
+        const val COLUMN_ID = "id"
+        const val COLUMN_EMAIL = "email"
+        const val COLUMN_PASSWORD = "password"
+    }
+}
